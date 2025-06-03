65a405490b2a792c50d86895d0c13d460e8cf0d1
Kevin Jonson <kevin@kevinjonson.com>
2025-06-03 14:26:54 -0700

    Remove prettier from project
    - Remove prettier dependencies and eslint-prettier integration
    - Delete prettier configuration files
    - Update VS Code settings and recommendations
    - Remove format script from package.json
    - Also remove obsolete GitHub Pages deployment workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,11 @@ module.exports = {
 	},
 	extends: [
 		'plugin:@typescript-eslint/recommended',
-		'prettier', // Use prettier config but don't enforce via ESLint
 	],
-	plugins: ['@typescript-eslint', 'prettier'],
+	plugins: ['@typescript-eslint'],
 	rules: {
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/no-explicit-any': 'warn',
 		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-		'prettier/prettier': ['error'], // This will error when prettier rules are violated
 	},
 };
